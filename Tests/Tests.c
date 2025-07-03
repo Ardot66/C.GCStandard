53e6ae044e1d327d03c3d6158e11280422920548
Gabriel Clay <gabrielwclay@gmail.com>
2025-07-03 11:10:01 +1000

    Updated tests to work with new testing library

--- Tests/Tests.c
+++ Tests/Tests.c
@@ -15,22 +15,22 @@ int TestList()
     for(int x = 0; x < 4; x++)
     {
         ListAdd(&list, &x);
-        TEST(list.V[x], ==, x, d)
-        TEST(list.Count, ==, x + 1, zu)
+        TEST(list.V[x], ==, x)
+        TEST(list.Count, ==, x + 1)
     }
 
     ListRemoveAt(&list, 0);
-    TEST(list.V[0], ==, 1, d)
+    TEST(list.V[0], ==, 1)
 
     ListClear(&list);
-    TEST(list.Count, ==, 0, zu)
+    TEST(list.Count, ==, 0)
 
     for(int x = 0; x < 4; x++)
     {
         ListInsert(&list, &x, 0);
 
         for(int y = 0; y <= x; y++)
-            TEST(list.V[y], ==, x - y, d)
+            TEST(list.V[y], ==, x - y)
     }
 }
 
