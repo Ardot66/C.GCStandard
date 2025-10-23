45b0b9c27e85fe970e13a566b6cf019afecd65b2
GabrielClay <gabrielwclay@gmail.com>
2025-10-23 14:11:57 +1100

    Prefixed math functions with GC to avoid conflicts with other headers

--- Header/GCMath.h
+++ Header/GCMath.h
@@ -1,8 +1,9 @@
 #ifndef __GC_MATH__
 #define __GC_MATH__
 
-#define min(a, b) ((a <= b) * a + (b < a) * b)
-#define max(a, b) ((a >= b) * a + (b > a) * b)
-#define clamp(a, min, max) (((a >= min) & (a <= max)) * a + (a < min) * min + (a > max) * max)
+// Prefixing with GC here because conflicts with other headers are very likely otherwise
+#define GCMin(a, b) ((a <= b) * a + (b < a) * b)
+#define GCMax(a, b) ((a >= b) * a + (b > a) * b)
+#define GCClamp(a, min, max) (((a >= min) & (a <= max)) * a + (a < min) * min + (a > max) * max)
 
 #endif
